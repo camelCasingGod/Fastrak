e6e5261339697343b0e23c791029cee6f0b82f64
Ameya Phadnis <aphadnis@berkeley.edu>
2022-11-06 06:38:45 -0800

    Worldcoin functionality added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { WorldIDWidget } from '@worldcoin/id'
+
 
 function App() {
+
+  const signal = "100";
+
+  function handleVerification(verificationResponse) {
+    console.log(verificationResponse);
+    let mRoot = verificationResponse.merkle_root;
+    let nHash = verificationResponse.nullifier_hash;
+    let proof = verificationResponse.proof;
+    console.log(signal);
+    // Fetch request would go here... localhost:3000 fetch request blocked by CORS
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        please verify using World ID to receive your venue pass
+        <br></br>
+        <br></br>
+        <WorldIDWidget
+          actionId="wid_staging_9c3b50f5324a3fff220f0b9222cf5521" // obtain this from developer.worldcoin.org
+          signal={signal}
+          enableTelemetry
+          onSuccess={(verificationResponse) => handleVerification(verificationResponse)}
+          onError={(error) => console.error(error)}
+        />
       </header>
     </div>
   );
