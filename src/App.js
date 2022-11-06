85ceca9dab7feef05db4163584d689ed08209a52
Ameya Phadnis <aphadnis@berkeley.edu>
2022-11-06 07:25:02 -0800

    Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { WorldIDWidget } from '@worldcoin/id'
+import { QRCodeCanvas } from "qrcode.react";
 
+var proof;
 
 function App() {
 
-  const signal = "100";
+  const signal = String(Math.random());
 
   function handleVerification(verificationResponse) {
     console.log(verificationResponse);
     let mRoot = verificationResponse.merkle_root;
     let nHash = verificationResponse.nullifier_hash;
     let proof = verificationResponse.proof;
-    console.log(signal);
+    console.log(mRoot);
+    console.log(nHash);
+    console.log(proof);
+    document.getElementById("qrCode").style.display = "block";
+    document.getElementById("verify").style.display = "none";
+    document.getElementById("pass").style.display = "block";
     // Fetch request would go here... localhost:3000 fetch request blocked by CORS
+    // I don't have access to an orb, so I didn't get a chance to verify my world ID for testing purposes
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        please verify using World ID to receive your venue pass
-        <br></br>
+        <p id="verify">Welcome! Please verify using World ID to receive your venue pass:</p>
         <br></br>
         <WorldIDWidget
           actionId="wid_staging_9c3b50f5324a3fff220f0b9222cf5521" // obtain this from developer.worldcoin.org
@@ -29,6 +36,16 @@ function App() {
           onSuccess={(verificationResponse) => handleVerification(verificationResponse)}
           onError={(error) => console.error(error)}
         />
+        <br></br>
+        <br></br>
+        <QRCodeCanvas
+          hidden
+          id="qrCode"
+          value={signal}
+          size={300}
+          level={"H"}
+        />
+        <p hidden id="pass">YOUR VENUE PASS</p>
       </header>
     </div>
   );
